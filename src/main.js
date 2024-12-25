0db44104ac553db1e87da9db794502e1ed07c655
itheima <orange123666888@163.com>
2024-12-25 22:11:33 +0800

    项目初始化-引入Vant租件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant核心组件库
+import Vant from 'vant'
+// 引入vant全局样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+// 注册使用Vant的组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
